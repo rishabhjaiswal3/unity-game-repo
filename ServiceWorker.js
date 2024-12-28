32d16591ab3c06ce47a44570877c7aa0d739e27e
rishabhjaiswal3 <rj838486@gmail.com>
2024-12-28 12:09:36 +0530

    added new project of unity

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "KultGames-BulletStorm-1.2.5";
+const contentToCache = [
+    "Build/BlueStormNew.loader.js",
+    "Build/BlueStormNew.framework.js",
+    "Build/BlueStormNew.data",
+    "Build/BlueStormNew.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
